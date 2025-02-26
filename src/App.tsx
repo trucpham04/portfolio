c42311e2011744b5d3f1f9c1a38ea7a48a457c36
Truc Pham <phamduytruc04@gmail.com>
2025-02-26 21:22:15 +0700

    refactor: Migrate project from Create React App to Vite and convert JSX to TSX

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import ReactGA from "react-ga4";
 
-import Homepage from "./pages/homepage";
-import About from "./pages/about";
-import Projects from "./pages/projects";
-import Articles from "./pages/articles";
-import ReadArticle from "./pages/readArticle";
-import Contact from "./pages/contact";
-import Notfound from "./pages/404";
+import Homepage from "./pages/homepage.tsx";
+import About from "./pages/about.tsx";
+import Projects from "./pages/projects.tsx";
+import Articles from "./pages/articles.tsx";
+import ReadArticle from "./pages/readArticle.tsx";
+import Contact from "./pages/contact.tsx";
+import Notfound from "./pages/404.tsx";
 
 import { TRACKING_ID } from "./data/tracking";
 import "./app.css";
